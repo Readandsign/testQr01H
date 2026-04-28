18cefd585763c4b488e89915d7981168895700ee
Readandsign <98477527+Readandsign@users.noreply.github.com>
2026-04-28 16:46:09 +0400

    Add variable declarations for DOM elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrp = document.querySelector(".wrp");
+const languageBlock = wrp.querySelectorAll(".languageBlock");
+const aside = wrp.querySelectorAll("aside");
+const info = wrp.querySelectorAll(".info");
+const a = wrp.querySelectorAll("a");
+const button = wrp.querySelectorAll(".button");
