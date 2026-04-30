7638b1628bb0f62d3f78e59fe06b9e3ba7ed1d80
Readandsign <98477527+Readandsign@users.noreply.github.com>
2026-05-01 03:07:09 +0400

    Update video ID and parameter validation logic

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const button = wrp.querySelectorAll(".button");
 const params = new URLSearchParams(window.location.search);
 
 const arr = [
-{id : "0000",
+{id : "1",
 videos : ["https://youtu.be/CCNz69pTqrY?si=Bkmd92OT4M2PcMXx", "https://youtu.be/h4q9tHf01A0?si=cLLWnetuyfnhDxfp", "https://youtu.be/_36bHzl-PV0?si=U-HwD1Fa-LkT3KvW",]},
 {id : "0001", videos : ["aaa", "bbb", "ccc"]},
 {id : "0002", videos : ["AAA", "BBB", "CCC"]}
@@ -16,7 +16,7 @@ videos : ["https://youtu.be/CCNz69pTqrY?si=Bkmd92OT4M2PcMXx", "https://youtu.be/
 
 Object.freeze(arr);
 
-if(params.get("id") != null && params.get("id").length === 4){
+if(params.get("id") != null && params.get("id").length < 5){
 for(let i = 0; i < arr.length; i++){
 if(params.get("id") === arr[i].id){
 for(let k = 0; k < 3; k++){
