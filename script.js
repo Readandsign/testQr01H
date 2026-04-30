5f1538e7e7c49819c4bc9597f70000ff4cdef1ff
Readandsign <98477527+Readandsign@users.noreply.github.com>
2026-04-30 21:16:59 +0400

    Implement link updates based on ID parameter
    Add functionality to update links based on URL parameters

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const aside = wrp.querySelectorAll("aside");
 const info = wrp.querySelectorAll(".info");
 const a = wrp.querySelectorAll("a");
 const button = wrp.querySelectorAll(".button");
+
+const params = new URLSearchParams(window.location.search);
+
+const arr = [
+{id : "0000", videos : ["link1", "link2", "link3"]},
+{id : "0001", videos : ["aaa", "bbb", "ccc"]},
+{id : "0002", videos : ["AAA", "BBB", "CCC"]}
+];
+
+Object.freeze(arr);
+
+if(params.get("id").length === 4){
+for(let i = 0; i < arr.length; i++){
+if(params.get("id") === arr[i].id){
+for(let k = 0; k < 3; k++){
+a[k].href = arr[i].videos[k];
+}
+}
+}
+}
