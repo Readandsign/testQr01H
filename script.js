9ae50b07e0254c5945aea3cddf77c31fb0927656
Readandsign <98477527+Readandsign@users.noreply.github.com>
2026-05-01 03:08:12 +0400

    Update video IDs and add new video entries

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const params = new URLSearchParams(window.location.search);
 const arr = [
 {id : "1",
 videos : ["https://youtu.be/CCNz69pTqrY?si=Bkmd92OT4M2PcMXx", "https://youtu.be/h4q9tHf01A0?si=cLLWnetuyfnhDxfp", "https://youtu.be/_36bHzl-PV0?si=U-HwD1Fa-LkT3KvW",]},
-{id : "0001", videos : ["aaa", "bbb", "ccc"]},
-{id : "0002", videos : ["AAA", "BBB", "CCC"]}
+{id : "2", videos : ["aaa", "bbb", "ccc"]},
+{id : "3", videos : ["AAA", "BBB", "CCC"]}
 ];
 
 Object.freeze(arr);
